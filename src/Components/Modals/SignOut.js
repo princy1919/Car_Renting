03bec929e0d57a596b9c333019a560bf3eca779a
princy1919 <sataniprincy2010@gmail.com>
2023-03-13 15:15:33 +0530

    chage the logout page

--- src/Components/Modals/SignOut.js
+++ src/Components/Modals/SignOut.js
@@ -1,32 +1,44 @@
-import React from "react";
-import {Modal} from "react-bootstrap";
-import {  Button } from 'reactstrap';
+import React, { useRef } from "react";
+import { Modal } from "react-bootstrap";
+import { Button } from 'reactstrap';
 
 export const SignOut = ({isLogout,signOut,handleCancel}) => {
-    return(
-        <Modal show={isLogout}>
-            <Modal.Body>
-                <div>
-                    <b>
-                        <p>
-                            Are you sure for logout ?
-                        </p>
-                    </b><br/>
-                </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <div>
-                    <a href="/" onClick={signOut}>
-                        <Button className='mr-2' color="success">
-                            Ok
-                        </Button>
-                    </a>
-                    <Button color="danger" onClick={handleCancel}>
-                        Cancel
-                    </Button>
-                </div>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+  const okButtonRef = useRef(null);
 
+  const handleCancelClick = () => {
+    okButtonRef.current = null;
+    handleCancel();
+  }
+
+  const handleModalShown = () => {
+    if (okButtonRef.current !== null) {
+      okButtonRef.current.click();
+    }
+  }
+
+  return(
+    <Modal show={isLogout} onShow={handleModalShown}>
+      <Modal.Body>
+        <div>
+          <b>
+            <p>
+              Are you sure for logout ?
+            </p>
+          </b><br/>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <div>
+          <a href="/" onClick={signOut}>
+            <Button innerRef={okButtonRef} className='mr-2' color="success">
+              Ok
+            </Button>
+          </a>
+          <Button color="danger" onClick={handleCancelClick}>
+            Cancel
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  )
+}
